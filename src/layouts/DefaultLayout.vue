ff33796f0a7eae0fb7d1198aff559c15dc410c8d
Eg0r0k <egokakill@gmail.com>
2025-10-14 20:39:35 +0300

    Add config files for docker and change vite config

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="flex h-full flex-col">
-        <div class="md:hidden h-14 border-b flex items-center justify-between px-3 bg-background">
-            <div class="text-sm">
-                <Scale />
+  <div class="flex h-full flex-col">
+    <div class="md:hidden h-14 border-b flex items-center justify-between px-3 bg-background">
+      <div class="text-sm">
+        <Scale />
+      </div>
+
+      <Drawer v-model:open="appStore.state.isDrawerOpen">
+        <DrawerTrigger asChild>
+          <Button class="size-8" variant="secondary" size="icon">
+            <MenuIcon />
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent class=" h-[85dvh] md:max-h-[500px]">
+          <DrawerHeader>
+            <div class="flex flex-col mt-2 gap-2">
+              <Button @click="appStore.closeDrawer" size="sm" class="justify-start " variant="ghost"
+                v-for="item in MAIN_NAV" :key="item.path" asChild>
+                <RouterLink :to="item.path" :title="item.labelKey"
+                  class="flex items-center justify-center text-muted-foreground/60 hover:text-foreground transition-colors">
+                  <component :is="item.icon" class="size-5" />
+                  {{ $t(item.labelKey) }}
+                </RouterLink>
+              </Button>
             </div>
-
-            <Drawer v-model:open="appStore.state.isDrawerOpen">
-                <DrawerTrigger asChild>
-                    <Button class="size-8" variant="secondary" size="icon">
-                        <MenuIcon />
-                    </Button>
-                </DrawerTrigger>
-                <DrawerContent class=" h-[85dvh] md:max-h-[500px]">
-                    <DrawerHeader>
-                        <div class="flex flex-col mt-2 gap-2">
-                            <Button @click="appStore.closeDrawer" size="sm" class="justify-start " variant="ghost"
-                                v-for="item in MAIN_NAV" :key="item.path" asChild>
-                                <RouterLink :to="item.path" :title="item.labelKey"
-                                    class="flex items-center justify-center text-muted-foreground/60 hover:text-foreground transition-colors">
-                                    <component :is="item.icon" class="size-5" />
-                                    {{ $t(item.labelKey) }}
-                                </RouterLink>
-                            </Button>
-                        </div>
-                    </DrawerHeader>
-                </DrawerContent>
-            </Drawer>
-        </div>
-
-        <MainHeader />
-        <div :inert="appStore.state.isDrawerOpen ? true : false" class="flex flex-1  w-full overflow-y-hidden ">
-            <MainSidebar v-if="!hideSidebar" />
-            <main id="main" class="flex-grow h-full overflow-y-auto">
-                <RouterView />
-            </main>
-        </div>
+          </DrawerHeader>
+        </DrawerContent>
+      </Drawer>
     </div>
+
+    <MainHeader />
+    <div :inert="appStore.state.isDrawerOpen ? true : false" class="flex flex-1  w-full overflow-y-hidden ">
+      <MainSidebar v-if="!hideSidebar" />
+      <main id="main" class="flex-grow h-full overflow-y-auto">
+        <RouterView />
+      </main>
+    </div>
+  </div>
 </template>
 
 
@@ -60,8 +60,8 @@ const appStore = useAppStore()
 <style scoped>
 .router-link-exact-active {
 
-    color: var(--foreground) !important;
-    background-color: var(--muted) !important;
+  color: var(--foreground) !important;
+  background-color: var(--muted) !important;
 
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       host: '0.0.0.0',
+      port: 8080,
       proxy: {
         '/api': {
           target: env.VITE_API_URL,
