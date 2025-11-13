d7cd1cc7062f464ad6f27cc0ddc6719ccc30d614
Eg0r0k <egokakill@gmail.com>
2025-11-13 15:45:29 +0300

    Position fix

--- src/pages/InvitePage.vue
+++ src/pages/InvitePage.vue
@@ -1,43 +1,32 @@
 <template>
-  <section class="flex pt-16 px-4 flex-col h-full w-full items-center">
-    <Card class="max-w-sm w-full">
-      <CardHeader>
-        <CardTitle class="flex flex-col items-center gap-4">
-          <Avatar class="size-28">
-            <AvatarImage draggable="false" src="https://github.com/unovue.png" alt="@unovue" />
-            <AvatarFallback class="text-sm">
-              <Loader2 class="animate-spin" />
-            </AvatarFallback>
-          </Avatar>
+  <section class="flex   p-4 flex-col h-full w-full justify-center items-center">
+    <Card class="relative w-full max-w-md shadow-md">
+
+      <CardHeader class=" flex flex-col items-center gap-4">
+        <Avatar class="size-24">
+          <AvatarImage draggable="false" src="https://github.com/unovue.png" alt="Group Avatar" />
+          <AvatarFallback>
+            G
+          </AvatarFallback>
+
+        </Avatar>
+
+        <CardTitle class="text-center text-2xl font-bold tracking-tight text-foreground break-words leading-tight">
+          asdasd
         </CardTitle>
-        <span
-          class="block w-full truncate scroll-m-20 text-center text-1xl font-extrabold tracking-tight lg:text-3xl"
-        >
-          NAME OF GROUPфывфывфывфы
-        </span>
+
+        <p class="text-sm text-muted-foreground text-center max-w-sm">
+          Организовано <span class="font-semibold text-foreground"> test </span>
+        </p>
+        <Badge class=" absolute top-4 right-3" variant="secondary">
+          120 Members
+        </Badge>
       </CardHeader>
       <CardContent class="space-y-4 text-muted-foreground">
-        <p class="text-sm text-center">
-          Присоединяйтесь к нашей группе и станьте частью нашего сообщества!
-        </p>
-        <ul class="space-y-2 text-sm">
-          <li class="flex items-center gap-2">
-            <CircleCheck class="h-5 w-5 text-green-500" />
-            Уникальные возможности для участников
-          </li>
-          <li class="flex items-center gap-2">
-            <CircleCheck class="h-5 w-5 text-green-500" />
-            Еженедельные обновления
-          </li>
-          <li class="flex items-center gap-2">
-            <CircleCheck class="h-5 w-5 text-green-500" />
-            Поддержка 24/7
-          </li>
-        </ul>
       </CardContent>
 
       <CardFooter>
-        <Button class="w-full"> JOIN </Button>
+        <Button class="w-full"> {{ $t('common.join') }} </Button>
       </CardFooter>
     </Card>
   </section>
@@ -47,11 +36,11 @@
 import Avatar from '@/components/ui/avatar/Avatar.vue'
 import AvatarFallback from '@/components/ui/avatar/AvatarFallback.vue'
 import AvatarImage from '@/components/ui/avatar/AvatarImage.vue'
+import Badge from '@/components/ui/badge/Badge.vue'
 import { Button } from '@/components/ui/button'
 import Card from '@/components/ui/card/Card.vue'
 import CardContent from '@/components/ui/card/CardContent.vue'
 import CardFooter from '@/components/ui/card/CardFooter.vue'
 import CardHeader from '@/components/ui/card/CardHeader.vue'
 import CardTitle from '@/components/ui/card/CardTitle.vue'
-import { CircleCheck, Loader2 } from 'lucide-vue-next'
 </script>
